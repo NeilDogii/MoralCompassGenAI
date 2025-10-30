b025f059c73345db5ff4814cea4cac03ff060c0d
NeilDogii <aryaworrior41@gmail.com>
2025-10-30 23:28:32 +0530

    feat(api): api route fix

--- src/lib/constants/URL.ts
+++ src/lib/constants/URL.ts
@@ -1,2 +1,2 @@
-// export const API_PREDICTION_URL = "http://localhost:8000/predict";
-export const API_PREDICTION_URL = "http://192.168.1.6:8000/predict";
+export const API_PREDICTION_URL = "http://localhost:8000/predict";
+// export const API_PREDICTION_URL = "http://192.168.1.6:8000/predict";
